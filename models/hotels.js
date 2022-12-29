66b38e73f921e9c646124a1637138ef7ea727ab0
gmussocoder <musso.guillermo.capacitacion@gmail.com>
2022-12-29 15:28:19 -0300

    29122022

---
+++ models/hotels.js
@@ -0,0 +1,13 @@
+var settings = require('../db/settings')
+
+
+var HotelsSchema = settings.mongoose.Schema(
+    {
+        /** No schema defined for hotels - Student may do it themselves */
+    },
+        /** remove the following if you define the schema */
+    { strict: false }
+);
+
+// Export the model
+exports.Hotels = settings.mongoose.model('hotels', HotelsSchema)
